38a0c829d7eaf4ea558519da69a4a6875fd9e0f2
takch02 <lim70kr38@gmail.com>
2026-02-01 20:35:50 +0900

    feat : 프로젝트 생성

---
+++ dosa_backend/src/main/java/com/test/dosa_backend/DosaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.test.dosa_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DosaBackendApplication.class, args);
+    }
+
+}
---
+++ dosa_backend/src/test/java/com/test/dosa_backend/DosaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.dosa_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DosaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
