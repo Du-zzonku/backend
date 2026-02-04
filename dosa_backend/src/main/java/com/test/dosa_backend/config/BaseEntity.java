156f1848bb5cd23c28d224a77633282f9edf27a1
guzzang <991012kim@gmail.com>
2026-02-04 11:42:22 +0900

    entity: model, part, node 객체 및 auditing 객체 생성

---
+++ dosa_backend/src/main/java/com/test/dosa_backend/config/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.test.dosa_backend.config;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity extends BaseTimeEntity {
+
+    @LastModifiedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+}
